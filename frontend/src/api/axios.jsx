d1eaff978ec2aad0c462e0618e08bae88c4adf42
Atuldubey98 <atuldubey017@gmail.com>
2023-02-01 12:30:54 +0530

    dev

--- frontend/src/api/axios.jsx
+++ frontend/src/api/axios.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "http://192.168.1.15:3000",
+  baseURL: "http://localhost:3000",
 });
 
 instance.interceptors.request.use(
--- frontend/src/components/Payment.jsx
+++ frontend/src/components/Payment.jsx
@@ -8,26 +8,54 @@ import {
   CLIENT_SECRET_ERROR,
   CLIENT_SECRET_LOADED,
   CLIENT_SECRET_LOADING,
+  STRIPE_PROMISE_ERROR,
+  STRIPE_PROMISE_LOADED,
+  STRIPE_PROMISE_LOADING,
 } from "../reducers/orderReducer";
 import CheckoutForm from "./CheckoutForm";
+import { loadStripe } from "@stripe/stripe-js";
 function Payment() {
   const { state, orderDispatch } = useContext(OrderContext);
   const { data: stripePromise } = state.stripePromise;
   const { data: clientSecret } = state.clientSecret;
+
   useEffect(() => {
-    try {
+    if (!stripePromise) {
       (async () => {
-        orderDispatch({ type: CLIENT_SECRET_LOADING });
-        const data = await OrderService.getPaymentIntent();
-        orderDispatch({
-          type: CLIENT_SECRET_LOADED,
-          payload: data.clientSecret,
-        });
+        try {
+          orderDispatch({ type: STRIPE_PROMISE_LOADING });
+          const { publishableKey } = await OrderService.getPublishableKey();
+          const payload = await loadStripe(publishableKey);
+          orderDispatch({
+            type: STRIPE_PROMISE_LOADED,
+            payload,
+          });
+        } catch (error) {
+          console.log(error);
+          orderDispatch({
+            type: STRIPE_PROMISE_ERROR,
+            payload: `Payment Error !`,
+          });
+        }
       })();
-    } catch (error) {
-      orderDispatch({ type: CLIENT_SECRET_ERROR, payload: "Payment Error" });
     }
-  }, []);
+  }, [stripePromise]);
+  useEffect(() => {
+    if (!clientSecret) {
+      try {
+        (async () => {
+          orderDispatch({ type: CLIENT_SECRET_LOADING });
+          const data = await OrderService.getPaymentIntent();
+          orderDispatch({
+            type: CLIENT_SECRET_LOADED,
+            payload: data.clientSecret,
+          });
+        })();
+      } catch (error) {
+        orderDispatch({ type: CLIENT_SECRET_ERROR, payload: "Payment Error" });
+      }
+    }
+  }, [clientSecret]);
   return (
     <div className="payment__wrapper">
       <h1>Payment</h1>
